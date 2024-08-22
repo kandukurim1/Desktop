c005aa661cd1a28e250df9ac63249da65dc0eda9
Manohar reddy <kandukuri.manoharreddy.com>
2024-08-22 10:42:42 -0700

    first file commit

---
+++ helloworld.py
@@ -0,0 +1,19 @@
+'''for num in  range(1,36)
+   if  num%5==0:
+    print("Fizz")
+   elif num%5==0 and num%10==0:
+    print("buzz")
+   else:
+    print(num)''' 
+
+
+for num in  range(1,36):
+   if  num%5==0 and num%10==0 :
+    print("Buzz")
+   elif num%5==0:
+    print("Fizz")
+   else:
+    print(num)     
+    
+    
+ 
